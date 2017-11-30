ee1a31e73801c72382776604dfc7a509eeacefdb
Dylan <dylan.renard.72@gmail.com>
2017-11-30 18:55:10 +0100

    merge

--- src/projetGO/Main.java
+++ src/projetGO/Main.java
@@ -6,15 +6,52 @@ public class Main {
 	{
 		Grille g = new Grille(13);
 		
+		g.jouerUnTour(3, 3);
+		System.out.println(g.getGrille()[3][3].getCouleur());
+		System.out.println(g.getGrille()[3][3].getGP().getNbLiberte());
+		
+		g.jouerUnTour(3, 4);
+		System.out.println(g.getGrille()[3][4].getCouleur());
+		System.out.println(g.getGrille()[3][3].getGP().getNbLiberte());
+		
+		g.jouerUnTour("passer");
+		System.out.println("passer");
+		
+		g.jouerUnTour(3, 2);
+		System.out.println(g.getGrille()[3][2].getCouleur());
+		System.out.println(g.getGrille()[3][3].getGP().getNbLiberte());
+		
+		g.jouerUnTour("passer");
+		System.out.println("passer");
+		
+		g.jouerUnTour(2, 3);
+		System.out.println(g.getGrille()[2][3].getCouleur());
+		
+		g.jouerUnTour("passer");
+		System.out.println("passer");
+		
+		g.jouerUnTour(4, 3);
+		System.out.println(g.getGrille()[4][3].getCouleur());
+		
+		System.out.println(g.caseOccupee(3, 3));
+		/*System.out.println(g.getGrille()[3][3].getCouleur());
+		System.out.println(g.getGrille()[3][3].getGP().getNbLiberte());*/
+		
+		System.out.println(g.getListeGroupe());
+		
+		
+		
+		
+		/*while(!g.getPartieFinie())
 		Fenetre fen = new Fenetre();
 		fen.pannel();
 		
 		while(!g.getPartieFinie())
 		{
-			//TODO déroulement d un tour de jeu
+			//TODO dï¿½roulement d un tour de jeu
 			
 			
-		}
+		}*/
 	}
 
 }
